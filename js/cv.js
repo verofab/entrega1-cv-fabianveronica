18b8b174dc9c18d2534f5299d92b2b5776ab9c40
verofab <103671727+verofab@users.noreply.github.com>
2023-03-08 23:56:12 -0300

    Add files via upload

---
+++ js/cv.js
@@ -0,0 +1,16 @@
+
+document.getElementById("boton1").addEventListener("click",function(){
+        document.getElementById('info1').style.display = 'block';   
+});
+
+document.getElementById("boton2").addEventListener("click",function(){
+    document.getElementById('info2').style.display = 'block';   
+});
+
+document.getElementById("boton3").addEventListener("click",function(){
+    document.getElementById('info3').style.display = 'block';   
+});
+
+document.getElementById("boton4").addEventListener("click",function(){
+    document.getElementById('info4').style.display = 'block';   
+});
